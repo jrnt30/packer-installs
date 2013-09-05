c70a3b515bc105d51694ccfa3bae37a603cb3bf0
jrnt30 <justin.r.nauman+github@gmail.com>
2013-09-05 18:13:15 -0500

    chore(*) - Cleanup code for aws - Removed vagrant specific settings for amazon-ebs - Corrected template.json to proper amazon name - removed cleanup.sh from aws - removed virtualbox.sh until can remove veewee specific configurations

--- centos64_puppet/scripts/cleanup.sh
+++ centos64_puppet/scripts/cleanup.sh
@@ -3,4 +3,7 @@ yum -y clean all
 rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
 rm -rf VBoxGuestAdditions_*.iso
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
-rm /etc/udev/rules.d/70-persistent-net.rules
+
+if [[ -f /etc/udev/rules.d/70-persistent-net.rules ]] then 
+  rm /etc/udev/rules.d/70-persistent-net.rules
+fi
--- centos64_puppet/scripts/puppet.sh
+++ centos64_puppet/scripts/puppet.sh
@@ -1,12 +1,14 @@
 # Install Puppet
 
-cat > /etc/yum.repos.d/puppetlabs.repo << EOM
-[puppetlabs]
-name=puppetlabs
-baseurl=http://yum.puppetlabs.com/el/6/products/\$basearch
-enabled=1
-gpgcheck=0
-EOM
+# cat > /etc/yum.repos.d/puppetlabs.repo << EOM
+# [puppetlabs]
+# name=puppetlabs
+# baseurl=http://yum.puppetlabs.com/el/6/products/\$basearch
+# enabled=1
+# gpgcheck=0
+# EOM
+
+rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
 
 yum -y install puppet facter
 
